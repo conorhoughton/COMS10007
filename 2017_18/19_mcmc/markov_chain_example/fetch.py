fa75e1ae14f2a6e07ec277eae8f5863bd4146124
NavyaZaveri <navyajayesh+1@gmail.com>
2018-06-07 19:48:57 +0400

    formatted fetch.py

--- 2017_18/19_mcmc/markov_chain_example/fetch.py
+++ 2017_18/19_mcmc/markov_chain_example/fetch.py
@@ -4,7 +4,7 @@ import re
 from PyLyrics import *
 
 BASE_URL = "http://www.songfacts.com/artist-{}.php"
-LIMIT = 45 #number of songs to fetch. Anything higher sadly leads to a request timeout 
+LIMIT = 45  # number of songs to fetch. Anything higher sadly leads to a request timeout
 
 
 def songs_by_artist(artist):
@@ -13,6 +13,7 @@ def songs_by_artist(artist):
     resp = requests.get(artist_url)
     html = bs.BeautifulSoup(resp.text, "lxml")
     song_titles = []
+
     for ul in html.find_all(class_="songullist-orange"):
         for song in ul.find_all("li")[:LIMIT]:
             song_titles.append(song.text)
@@ -25,10 +26,16 @@ def extract_song_from_song_title(artist, song_titles):
     songs = []
     for song_title in song_titles:
         try:
+
             lyrics = PyLyrics.getLyrics(artist, song_title).splitlines()
+
+            #a quick hack to preprocess the text 
             clean_lyrics = [
                 re.sub("[^a-z' ]", "", line.lower().strip()) for line in lyrics]
+
             songs.append(clean_lyrics)
+
         except ValueError:
             pass
+            
     return songs
